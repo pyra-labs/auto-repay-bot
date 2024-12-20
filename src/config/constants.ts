c7328cffba8927c291cef9582db0285616da3fb2
iarla-crewe <iarla@quartzpay.io>
2024-12-20 00:35:00 +0000

    Update package.json to new SDK

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,7 +4,7 @@ export const MIN_LAMPORTS_BALANCE = 0.001 * LAMPORTS_PER_SOL;
 export const LOOP_DELAY = 30_000;
 export const MAX_AUTO_REPAY_ATTEMPTS = 3;
 
-export const GOAL_HEALTH = 20;
+export const GOAL_HEALTH = 15;
 export const JUPITER_SLIPPAGE_BPS = 50;
 export const DRIFT_SOL_COLLATERAL_WEIGHT = 0.9;
 export const MARGINFI_FLASH_LOAN_TOKEN = "SOL";
